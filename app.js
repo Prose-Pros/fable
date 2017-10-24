97a2f8d1a5c2605f131b5b8d383990dcfae1a2d7
dfrancese <davefrancese@gmail.com>
2017-10-24 09:25:28 -0600

    homepage/added images folder/syle.css

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
-
+app.use(express.static('public'))
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+
 
 app.get('/', (req,res) => {
   res.render('index', {
