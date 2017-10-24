26a312d762c8a6bd18869b0bf3a0729156d00f2a
Mike <you@example.com>
2017-10-24 09:58:10 -0600

    Put static file in app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
-
+app.use(express.static('public'))
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
