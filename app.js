66458c8e4e906d5b25fee34f02431ddc192fba8d
Mike <you@example.com>
2017-10-25 14:11:29 -0600

    added the randomly generated quote to the index page

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const queries = require('./db/queries')
 const routes = require('./routes/createLogin')
 const prompts = require('./prompts')
 
-let loggedIn = false;
-let currentUser = "";
-
 
 app.set('view engine', 'hbs');
 app.use(express.static('public'))
@@ -20,28 +17,18 @@ app.use(bodyParser.json())
 app.get('/', (req,res) => {
   queries.getGenres()
   .then(dataGenres => {
-    if (loggedIn === false) {
-      res.render('index', {
-        title: 'Fable',
-        dataGenres: dataGenres,
-        loggedIn: loggedIn,
-      })
-    } else {
-      res.render('index', {
-        title: 'Fable',
-        dataGenres: dataGenres,
-        loggedIn: loggedIn,
-        currentUser: currentUser,
-      })
-    }
+    res.render('index', {
+      title: 'Fable',
+      dataGenres: dataGenres
+    })
   })
 })
 
 app.post('/:login', (req,res) => {
   queries.createAccount(req.body)
-  .then(function(code){
+  .then(function(user){
     res.render('login', {
-      code: code
+      test: user
     })
   })
   .catch(function(err){
@@ -49,62 +36,27 @@ app.post('/:login', (req,res) => {
   })
 })
 
-
-app.post('/login/user', (req,res)=>{
-  const username = req.body.username;
-  const code = req.body.code;
-  queries.login(username)
-  .then(userInfo => {
-    if(userInfo[0].code == code) {
-      loggedIn = true;
-      currentUser = userInfo[0]
-      res.redirect('/')
-    } else {
-      res.sendStatus(401);
-    }
-  })
-  .catch(err => {
-    res.sendStatus(500)
-  })
-})
-
-
 app.get('/login', (req,res)=>{
   res.render('login')
 })
 
-app.get('/write', (req,res) => {
-  queries.getGenres()
-  .then(dataGenres => {
-    res.render('write', {
-      dataGenres: dataGenres,
-      currentUser: currentUser
-    })
-
-  })
-})
-
-app.post('/write/createStory', (req,res) => {
-  queries.newStory(req.body)
-  .then(function(story){
-    res.redirect('/')
-  })
-})
-
-
 app.get('/:genre', (req,res)=> {
   const genre = req.params.genre
   queries.getStories(genre)
-  .then(writers => {
-    res.render('genre', {
-      genre: genre,
-      writers: writers
-    })
-  })
+  // .then(stories => {
+  //   for (var i = 0; i < stories.length; i++) {
+  //     const author = stories[i].user_id;
+  //     queries.getAuthor(author)
+      .then(writers => {
+        res.render('genre', {
+          genre: genre,
+          writers: writers
+        })
+
+      })
+  //   }
+  // })
 })
-
-
-
 
 app.listen(port, () => {
   console.log('Listening on port:', port);
--- public/index.js
+++ public/index.js
@@ -220,16 +220,15 @@ var obliqueStrategies = [
 // ---- **** QUOTE ON HOME PAGE **** ----
 
 
-// fetch('https://talaikis.com/api/quotes/random/')
-// .then(function(res) {
-//   return res.json()
-//   .then(function(quoteData){
-//     // console.log(quoteData)
-//
-//       var quotePara = document.getElementById('quote')[0]
-//       var randomQuote = quoteData.quote
-//       var quoteAuthor = quoteData.author
-//       quotePara.innerText = (randomQuote, quoteAuthor)
-//
-//   })
-// })
+fetch('https://talaikis.com/api/quotes/random/')
+.then(function(res) {
+  return res.json()
+  .then(function(quoteData){
+    console.log(quoteData)
+    var pQuote = document.getElementById('quote')
+
+    pQuote.innerText = quoteData.quote + ' - ' + quoteData.author
+    console.log(pQuote);
+
+  })
+})
